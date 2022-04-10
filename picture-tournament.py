e10dcd5e6754dcac7d40a1b5308f8ae8461c8060
jwav <wavresky.julien@gmail.com>
2022-04-10 14:46:05 +0200

    set the general goals

---
+++ picture-tournament.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# get list of pictures
+# draw random pairs
+# for pair in pairs
+#  display duel, handle key input
+#  update scores (for now: +1/-1)
+# at the end: save scores in file
+
+
