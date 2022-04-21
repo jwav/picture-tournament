328b6cf6e7e8bd552c0397503b7dc3bff26cc562
jwav <wavresky.julien@gmail.com>
2022-04-21 17:44:32 +0200

    made some progress on saves

--- picture-tournament.py
+++ picture-tournament.py
@@ -12,14 +12,15 @@ import logging
 import cv2
 import itertools
 import os
+import json
 
-# TODO
+
+
 class Picture:
     """Represents a picture with a filename and a score"""
     def __init__(self, filename, score=0):
         self.filename = filename
         self.score = score
-
     
     @property
     def name(self):
@@ -27,7 +28,6 @@ class Picture:
         if "." not in self.filename:
             return self.filename
         return self.filename.split(".")[0]
-
 
     @classmethod
     def get_ext(cls, filename):
@@ -42,20 +42,26 @@ class Picture:
         else:
             return cv2.imread(os.path.join(directory, self.filename))
 
-    
-
 
 class PictureList(list):
     """a list[Picture] instance with custom methods"""
+    DEFAULT_SAVE_FILENAME = "default.ptsave"
 
     def to_text(self) -> str:
         return "\n".join([f"{p.filename} : {p.score}" for p in self])
 
-    def load_from_savefile(self, save_file_name):
-        with open(save_file_name, "r") as f:
-            for line in f.readlines():
-                self.update_from_text_line()
+    def save_to_file(self, filename):
+        with open(filename, "w") as f:
+            f.write(self.to_text())
+            logging.info(f"saved PictureList as {filename}")
 
+    # TESTME
+    def load_from_savefile(self, filename):
+        with open(filename, "r") as f:
+            for line in f.readlines():
+                self.update_from_text_line(line)
+
+    # TESTME
     def update_from_text_line(self, line:str):
         try:
             assert ":" in line
@@ -66,24 +72,34 @@ class PictureList(list):
         except:
             logging.error("Failed to update picture list from '{}'".format(line))
 
+    # TESTME
     def remove_picture(self, filename):
         matches = [p for p in self if p.filename == filename]
         if matches:
             self.remove(matches[0])
 
+    def sort_by_score(self):
+        self.sort(key=lambda pic:pic.score)
+
+    def sort_by_filename(self):
+        self.sort(key=lambda pic:pic.filename)
+
+    #TESTME
+    def get_copy_sorted_by_score(self):
+        ret = PictureList(self)
+        ret.sort_by_score()
+        return ret
 
 
     @property
     def length(self):
         return len(self)
 
-
     @classmethod
     def is_img_file(cls, filename):
         extensions = ["jpg", "png", "gif"]
         return Picture.get_ext(filename) in extensions
 
-    #TODO
     def random_pairs(self,
             nb_pairs=None,
             no_duplicate=True,
@@ -97,17 +113,38 @@ class PictureList(list):
             else:
                 returns an amount of pairs equal to len(self)
         """
-        pairs = list(itertools.combinations(self, 2))[:nb_pairs]
-        return pairs
+        import random
+        
+        if no_duplicate:
+            idlist = list(range(self.length))
+            pairs = []
+            random.shuffle(idlist)
+            print(idlist)
+
+            while True:
+                if not idlist:
+                    break
+                if nb_pairs is not None and len(pairs) >= nb_pairs:
+                    break
+
+                if len(idlist) >= 2:
+                    ida,idb = idlist.pop(), idlist.pop()
+                    pairs.append([self[ida], self[idb]])
+                elif len(idlist) == 1:
+                    if avoid_leftalones:
+                        ida,idb = idlist.pop(), random.randint(0, self.length)
+                        pairs.append([self[ida], self[idb]])
+            return pairs
+        else:
+            # TODO: what does this do, already? write it down
+            pairs = list(itertools.combinations(self, 2))[:nb_pairs]
+            return pairs
     
-    #TODO
     def load_from_directory(self, directory):
         """loads itself from the pictures contained in a directory"""
         from os import listdir, path
         logging.debug(f"loading pictures from {directory}")
         # print(listdir(directory))
-
-        
 
         filenames = [f for f in listdir(directory) if self.is_img_file(f)]
 
@@ -123,6 +160,7 @@ class PictureList(list):
 # TODO: check cointoss.GameParams, write a generic module from it and import it here
 class Params:
     """A holder/manager class for the parameters provided by the user via the CLI"""
+
     def __init__(self):
         # default params
         # self.max_rounds = None
@@ -132,29 +170,43 @@ class Params:
         self.rightkey = "l"
         self.drawkey = "k"
         self.exitkey = "q"
+        self.exitnosavekey = "z"
+        self.displayscoreskey = "d"
 
     def update_from_argparse_args(self, args):
         """Updates this instance from an `arg` object returned by argparse.ArgumentParser.parse_args()"""
         self.__dict__.update(args.__dict__)
 
-# TODO
+# TODO display user interface keys on the image
 class Tournament:
+    """Main class meant to be initialized and run in the main function"""
+
     def __init__(self):
         self.params = Params()
         self.pictures = PictureList()
-        self.quicksave_filename = "quicksave.ptsave"
+        self.DEFAULT_SAVE_FILENAME = "quicksave.ptsave"
+
+    def print_scores(self):
+        print("<Scores>")
+        print(self.pictures.to_text())
+        print("</Scores>")
 
     def clean_exit(self):
         self.quicksave()
         exit(0)
 
-    #TODO
+    #testme
     def save_to_file(self, filename):
-        pass
+        self.pictures.save_to_file(filename)
+
+    #testme
+    def save_to_file(self, filename):
+        self.pictures.save_to_file(filename)
 
     def quicksave(self):
-        self.save_to_file(self.quicksave_filename)
-
+        import time
+        timestr = time.strftime("%Y-%m-%d_%H-%M-%S")
+        self.save_to_file("{}.ptsave".format(timestr))
 
     def handle_cli_args(self):
         """parses the cli arguments and sets global program parameters accordingly"""
@@ -165,7 +217,7 @@ class Tournament:
         args = parser.parse_args()
         self.params.update_from_argparse_args(args)
 
-    #TODO
+    # TODO display user interface keys on the image
     def show_duel(self, pic_list:list[Picture]):
         win_height = 200
         win_width = win_height * 16/9
@@ -187,8 +239,9 @@ class Tournament:
         duel_img = cv2.hconcat(im_list_resize)
 
         duel_text = " vs ".join(pic.name for pic in pic_list)
+        window_title = f"picture-tournament - {duel_text}"
 
-        cv2.imshow(duel_text, duel_img)
+        cv2.imshow(window_title, duel_img)
         while True:
             key = cv2.waitKey(0)
 
@@ -205,6 +258,12 @@ class Tournament:
             elif key == ord(self.params.exitkey):
                 self.clean_exit()
                 break
+            elif key == ord(self.params.exitnosavekey):
+                exit(0)
+                break
+            elif key == ord(self.params.displayscoreskey):
+                print(self.pictures.to_text())
+                break
             else:
                 logging.warning(f"unknown key: {key}")
                 continue
@@ -212,30 +271,41 @@ class Tournament:
 
         cv2.destroyAllWindows()
 
-    #TODO
-    def run(self):
+    #TODO: handle other methods than rounds/matches
+    def run(self, nb_rounds, nb_matches=None):
+        """main method, runs the tournament by asking the user to choose between two pictures.
+        Args:
+            - nb_rounds: number of rounds to go for
+            - nb_matches: max number of matches for each round. If None, does the max amount of matches
+        """
 
-        logging.debug("starting run()")
+
+        logging.debug("<Tournament.run()>")
         self.pictures.load_from_directory(self.params.directory)
         logging.debug("with {} pictures".format(self.pictures.length))
 
-
-        while True:
+        duels_done = 0
+        for round_id in range(nb_rounds):
+            logging.info("ROUND {}".format(round_id))
             pairs = self.pictures.random_pairs(
                     nb_pairs=None,
-                    no_duplicate=True,
+                    no_duplicate=False,
                     avoid_leftalones=True)
-            for pair in pairs:
-                logging.info("dueling {} vs {}".format(pair[0].name, pair[1].name))
+            if nb_matches is None:
+                nb_matches = len(pairs)
+            # logging.debug("result of random_pairs(): {}".format(
+            #     "\n".join([str(x) for x in pairs])))
+            logging.debug("result of random_pairs(): {}".format(pairs))
+            logging.debug("/result")
+            for match_id,pair in enumerate(pairs):
+                total_duels = nb_matches * nb_rounds
+                duels_pct = int(100 * (duels_done / total_duels))
+                logging.info("[{}%] round {}/{}, match {}/{}: dueling {} vs {}".format(duels_pct, round_id, nb_rounds, match_id, nb_matches, pair[0].name, pair[1].name))
                 self.show_duel(pair)
+                duels_done += 1
+            self.quicksave()
 
-            break
-
-
-
-
-
-
+        logging.debug("</Tournament.run()>")
 
 def main():
     print("== Picture Tournament begin ==")
@@ -244,7 +314,8 @@ def main():
     t = Tournament()
     t.handle_cli_args()
     t.params.directory = "images"
-    t.run()
+    t.run(nb_rounds=2, nb_matches=None)
+    t.print_scores()
 
     print("== Picture Tournament end ==")
 
